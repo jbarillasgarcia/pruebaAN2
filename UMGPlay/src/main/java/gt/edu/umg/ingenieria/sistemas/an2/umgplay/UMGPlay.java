04db814934f17cff1b2fdfd8d815b5dad9e3242f
joshi <joshi@SCAR2>
2019-09-26 12:41:34 -0600

    Versión inicial de UMGPlay.

---
+++ UMGPlay/src/main/java/gt/edu/umg/ingenieria/sistemas/an2/umgplay/UMGPlay.java
@@ -0,0 +1,9 @@
+package gt.edu.umg.ingenieria.sistemas.an2.umgplay;
+
+public class UMGPlay {
+    
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a UMGPlay");
+    }
+    
+}
